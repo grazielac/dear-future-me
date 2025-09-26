02516efc13b86ac20e09eab9ba39221049132454
grazielac <cpgraziela@gmail.com>
2025-09-27 00:30:43 +0200

    Add homepage

--- server/seed.js
+++ server/seed.js
@@ -1,12 +1,13 @@
+// This is usually a script to populate your database with initial or dummy data.
+// run: node seed.js
+// it doesn't start the server, just inserts data
+import express from "express";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = new pg.Pool({
-  connectionString: process.env.DB_CONN_STRING,
+  connectionString: process.env.DB_CONN,
 });
 
-db.query(
-  `INSERT INTO letters (name, email, message)
-VALUES
-('Kent', 'alice@mail.com', 'Hi future me!'),
-('Chie', 'bob@mail.com', 'Don’t forget your goals');`
-);
