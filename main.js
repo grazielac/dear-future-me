c6600000d8f697fdb1dca7806e1210c4ef6794e7
grazielac <cpgraziela@gmail.com>
2025-09-29 00:04:23 +0200

    Copy main files to root folder

---
+++ main.js
@@ -0,0 +1,102 @@
+// custom code for frontend
+const form = document.getElementById("letterForm");
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const messageError = document.getElementById("messageError");
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  // clear previous errors
+  nameError.textContent = "";
+  emailError.textContent = "";
+  messageError.textContent = "";
+
+  // collects the user's input from html form and turn it into a js object
+  const formData = new FormData(form); // pass 'form' element and collects all input values inside that form
+
+  // get the values from each input form
+  const name = form.name.value.trim(); //trim remove extra spaces start and end
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+
+  // VALIDATION ERROR
+  let error = false; // use later to stop the form submission
+
+  // FORM VALIDATION
+  // must not be empty!
+  if (name === "") {
+    nameError.textContent = "Name is required.";
+    error = true;
+  }
+  if (email === "") {
+    emailError.textContent = "Email is required.";
+    error = true;
+  }
+
+  // MESSAGE CHECK: min 20 words, max 100 words
+  const words = message.split(" ").filter((word) => word !== "");
+  // splits the mssage string intro an array of words using space as seperator
+  if (words.length < 5) {
+    // counts how many words are in the array
+    messageError.textContent = "Write at least 5 words."; // if user wrote less than 5
+    error = true;
+  } else if (words.length > 100) {
+    // checks if the user wrote more than 100 words
+    messageError.textContent = "Max 100 words allowed."; // if yes, show error message
+    error = true; // stop submission
+  }
+  //
+  if (error) return;
+
+  const data = { name, email, message };
+  // sends a POST request to my express server
+  try {
+    const res = await fetch("http://localhost:3000/letters", {
+      method: "POST", // send new data
+      headers: { "Content-Type": "application/json" }, // tells the server that the body of the request is json
+      body: JSON.stringify(data), // converts js object 'data; to a json string
+    });
+
+    const result = await res.json();
+    console.log(result);
+
+    if (res.ok) {
+      alert("Letter sent!");
+      form.reset();
+      loadLetters(); // refresh the displayed letters
+    } else {
+      alert("Something went wrong. Please try again.");
+    }
+  } catch (err) {
+    console.error(err);
+    alert("Error sending letter");
+  }
+});
+
+// GET DATA FROM THE SERVER and display it in the page
+async function loadLetters() {
+  try {
+    // call server GET route
+    const res = await fetch("http://localhost:3000/letters"); // sends a GET request to backend at /letters
+    const letters = await res.json(); // response body converts from json -> javascript object/array
+
+    // find the container
+    const list = document.getElementById("lettersList");
+    list.innerHTML = ""; // clear old content
+
+    // display each letter into the page
+    letters.forEach((letter) => {
+      const li = document.createElement("li");
+      li.textContent = `${letter.name}: ${letter.message}`; // show name + message
+      list.appendChild(li); // add li into the list
+    });
+  } catch (error) {
+    console.error("Error fetching letters:", error);
+  }
+  console.log(list);
+}
+
+loadLetters(); // run the function when the page loads
+
+// style LIST
